2845c225b76d1285125c5105029c20c438d66ed3
GreeNox <saydulla2410@gmail.com>
2024-08-14 14:00:35 +0500

    Add App icons and images address

--- lib/style/app_icons.dart
+++ lib/style/app_icons.dart
@@ -2,4 +2,24 @@ class AppIcons {
 
   AppIcons._();
 
+  static const String arrowBack = "assets/images/icons/arrow_back.png";
+  static const String arrowForward = "assets/images/icons/arrow_forward.png";
+  static const String menu = "assets/images/icons/bars.png";
+  static const String moreIcon = "assets/images/icons/dots.png";
+  static const String edit = "assets/images/icons/edit.png";
+  static const String facebook = "assets/images/icons/facebook.png";
+  static const String globe = "assets/images/icons/globe.png";
+  static const String globeActive = "assets/images/icons/globe_active.png";
+  static const String google = "assets/images/icons/google.png";
+  static const String like = "assets/images/icons/like.png";
+  static const String likeActive = "assets/images/icons/like_active.png";
+  static const String magnetic = "assets/images/icons/magnetic.png";
+  static const String profile = "assets/images/icons/profile.png";
+  static const String rocket = "assets/images/icons/rocket.png";
+  static const String ruler = "assets/images/icons/ruler.png";
+  static const String sun = "assets/images/icons/sun.png";
+  static const String temperature = "assets/images/icons/temperature.png";
+  static const String twitter = "assets/images/icons/twitter.png";
+  static const String user = "assets/images/icons/user.png";
+
 }
