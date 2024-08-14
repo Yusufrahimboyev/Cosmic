d8f0f103d158b6862dd31881c5e78da58cbe90b8
Muhammadyusuf <Muhammadyusufsotvoldiyev020@gmail.com>
2024-08-14 16:30:58 +0500

    Splash ready

--- lib/screens/login/login_screen.dart
+++ lib/screens/login/login_screen.dart
@@ -1,10 +1,26 @@
+import 'package:cosmic_app/style/app_images.dart';
 import 'package:flutter/material.dart';
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
 
   @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: DecoratedBox(
+        child: SizedBox.expand(),
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            fit: BoxFit.cover,
+            image: AssetImage(AppImages.backImagesFirst),
+          ),
+        ),
+      ),
+    );
   }
 }
--- lib/screens/splash/splash_screen.dart
+++ lib/screens/splash/splash_screen.dart
@@ -1,16 +1,99 @@
+import 'package:cosmic_app/router/router_names.dart';
 import 'package:cosmic_app/style/app_images.dart';
 import 'package:flutter/material.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = AnimationController(
+      vsync: this,
+      duration: Duration(seconds: 3),
+    );
+    _controller.forward();
+    Future.delayed(Duration(seconds: 4),() => Navigator.pushReplacementNamed(context, RouterNames.login),);
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: DecoratedBox(
-        child: Text(""),
-        decoration: BoxDecoration(
+        child: Stack(
+          children: [
+            Center(
+                child: AnimatedBuilder(
+                    animation: _controller,
+                    builder: (context, child) {
+                      return SizedBox(
+                        height: 330,
+                        width: 330,
+                        child: CircularProgressIndicator(
+                          color: Colors.white,
+                          value: _controller.value,
+                          strokeWidth: 10,
+                        ),
+                      );
+                    })),
+            const Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Center(
+                  child: Text(
+                    "cosmic",
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 75,
+                        color: Colors.white),
+                  ),
+                ),
+                Center(
+                  child: Text(
+                    "Free Mobile App UI Template",
+                    style: TextStyle(fontSize: 12, color: Colors.white),
+                  ),
+                )
+              ],
+            ),
+            const Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    FlutterLogo(
+                      size: 35,
+                    ),
+                    Text(
+                      "Flutter",
+                      style: TextStyle(fontSize: 30, color: Colors.white),
+                    )
+                  ],
+                ),
+                SizedBox(height: 72),
+              ],
+            ),
+          ],
+        ),
+        decoration: const BoxDecoration(
           image: DecorationImage(
+            fit: BoxFit.cover,
             image: AssetImage(AppImages.backImagesFirst),
           ),
         ),
